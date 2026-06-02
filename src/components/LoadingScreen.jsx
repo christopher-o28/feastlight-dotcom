72a64b01da99afb36a356ee2d2e27199eb36ccbb
christopher-o28 <christopher.o@svrtv.com>
2026-06-02 15:10:08 +0800

    fix

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -22,7 +22,7 @@ export default function LoadingScreen() {
           <div className="w-20 h-20 bg-gradient-to-br  to-feast-red/80 rounded-full flex items-center justify-center
                           ">
             <img
-              src="https://feastlight.com/wp-content/uploads/2022/01/Feast-Light-Logo-x152.png"
+              src="https://tfvtalks.s3.dualstack.ap-southeast-1.amazonaws.com/1%20The%20FeastLight%20Archives%20/Images%20/Feast-Light-Logo-Rebrand-FINAL.png"
               alt="Feast Light Logo"
               className="animationLogoLink w-50 h-50 object-contain"
             />
